1a01a7044d196b8004ea846bfadc412f436ec549
Natalia <markina_n@resanta.ru>
2023-04-12 21:41:01 +0300

    add info

---
+++ index.js
@@ -0,0 +1,9 @@
+// Код для первого задания напиши в этой функции
+function crawlTree() {}
+
+// Код для второго задания напиши в этой функции
+function twoSum() {}
+
+// Всё, что ниже, нужно для тестов. Не изменяй этот код
+const functions = { crawlTree, twoSum };
+module.exports = functions;

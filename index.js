baf0731df36b3617cf8ad7d27dddf0b9ca71b590
Svetlana Govyazina <govyazina@gmail.com>
2023-04-13 19:44:53 +0300

    first function added

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 // Код для первого задания напиши в этой функции
-function crawlTree() {}
+function crawlTree(arr) {
+    let result = ''
+    for (const arrElement of arr) {
+        if (!Array.isArray(arrElement)) {
+             result += arrElement
+        } else {
+             result += crawlTree(arrElement)
+        }
+    }
+    return result
+}
+
+
+
+
+
 
 // Код для второго задания напиши в этой функции
 function twoSum() {}
